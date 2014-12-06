9277328c5664885740aabac1a2d1e79ea652c70f
hongsimon <hsm10730@gmail.com>
2014-12-06 22:09:02 +0900

    MainFrame

---
+++ src/constants/GEConstants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class GEConstants {
+	//GEMainFrame
+	public static final int WIDTH_MAINFRAME = 400;
+	public static final int HEIGHT_MAINFRAME = 600;
+	public static final String TITLE_MAINFRAME = "그래픽 에디터-01";
+}
